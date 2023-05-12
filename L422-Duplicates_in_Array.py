11fd47300288ba6af0717be2e8e50f0a45cf8d7d
sterf-mac <nakamata@Stephanies-MacBook-Air-2.local>
2023-05-12 13:20:31 -0400

    Optimal solution using marking of item at index

--- L422-Duplicates_in_Array.py
+++ L422-Duplicates_in_Array.py
@@ -4,15 +4,25 @@ L422 - Duplicates_in_Array
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         duplicates = []
-        for i in range(3):
-            for idx in range(len(nums)):
-                nums[nums[idx] - 1], nums[idx] = nums[idx], nums[nums[idx] - 1]
-        print(nums)
         for idx in range(len(nums)):
-            if idx != nums[idx] - 1 and nums[nums[idx] - 1] == nums[idx]:
-                duplicates.append(nums[idx])
+            if nums[abs(nums[idx]) - 1] < 0:
+                duplicates.append(abs(nums[idx]))
+            nums[abs(nums[idx]) - 1] = -nums[abs(nums[idx]) - 1]
+
         return duplicates
 
+
+# class Solution:
+#     def findDuplicates(self, nums: List[int]) -> List[int]:
+#         duplicates = []
+#         for i in range(3):
+#             for idx in range(len(nums)):
+#                 nums[nums[idx] - 1], nums[idx] = nums[idx], nums[nums[idx] - 1]
+#         print(nums)
+#         for idx in range(len(nums)):
+#             if idx != nums[idx] - 1 and nums[nums[idx] - 1] == nums[idx]:
+#                 duplicates.append(nums[idx])
+#         return duplicates
 
 # time O(n) - 4 passes through solution... what is the dependence of size of nums and number of times we have to rearrange the items?
 # space O(n) - array solution to be returned depends on nums
