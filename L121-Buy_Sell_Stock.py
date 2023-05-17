d6301db594d61677ce9979a67df668e3acadd916
sterf-mac <nakamata@Stephanies-MacBook-Air-2.local>
2023-05-17 18:30:00 -0400

    Good solution

---
+++ L121-Buy_Sell_Stock.py
@@ -0,0 +1,15 @@
+L121 - Buy_Sell_Stock.py
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lowest_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            lowest_price = price if price < lowest_price else lowest_price
+            max_profit = (
+                (price - lowest_price)
+                if (price - lowest_price) > max_profit
+                else max_profit
+            )
+        return max_profit
