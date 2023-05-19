101a967cca87ad60d6238b8fda8281bb9b5cfc04
sterfd-windows <stefhwin@gmail.com>
2023-05-19 18:15:47 -0400

    Two pointer solution same time and space

--- L19-Remove_Nth_from_End_List.py
+++ L19-Remove_Nth_from_End_List.py
@@ -3,23 +3,36 @@ L19 - Remove_Nth_from_End_List.py
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length = 1
-        node = head
-        while node.next:
-            node = node.next
-            length += 1
+        # TWO POINTERS
+        prev = end = head
+        for _ in range(n):
+            end = end.next
+        if not end:
+            return head.next
+        while end.next:
+            prev, end = prev.next, end.next
+        prev.next = prev.next.next
 
-        if length == 1:
-            return
-        elif length == n:
-            head = head.next
-            return head
-
-        node = head
-        for i in range(0, length - n - 1):
-            node = node.next
-        node.next = node.next.next if n > 1 else None
         return head
+
+        # TWO PASS SOLUTION
+        # length = 1
+        # node = head
+        # while node.next:
+        #     node = node.next
+        #     length += 1
+
+        # if length == 1:
+        #     return
+        # elif length == n:
+        #     head = head.next
+        #     return head
+
+        # node = head
+        # for i in range(0, length - n - 1):
+        #     node = node.next
+        # node.next = node.next.next if n > 1 else None
+        # return head
 
     # time O(n) - traverse list twice
     #  space O(1) - intialize a pointer and an int
