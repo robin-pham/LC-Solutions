2edb28d272def289ffdce7846f44abc963a1d5e2
sterf-mac <stefhwin@gmail.com>
2023-05-25 15:01:07 -0400

    shorter code with list comprehension added same time and space

--- L784-Letter_Permutations.py
+++ L784-Letter_Permutations.py
@@ -3,8 +3,22 @@ L784 - Letter_Permutations.py
 
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        # there will be 2^N permutations where N is the number of letters in s
+        permutations = [""]
+        for ch in s:
+            if ch.isalpha():
+                permutations = [
+                    perm + letter
+                    for perm in permutations
+                    for letter in [ch, ch.swapcase()]
+                ]
+            else:
+                permutations = [perm + ch for perm in permutations]
+        return permutations
 
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        # there will be 2^N permutations where N is the number of letters in s
         permutations = [""]
         for ch in s:
             new_perms = []
