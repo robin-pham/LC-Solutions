9f83e74575c741121db608003a21e09541c9a4dd
sterf-mac <stefhwin@gmail.com>
2023-07-06 16:30:11 -0400

    initial solution

---
+++ L208-Implement_Trie.py
@@ -0,0 +1,36 @@
+L208 - Implement_Trie
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.ends = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.ends = True
+
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.ends
+
+    def startsWith(self, prefix):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
