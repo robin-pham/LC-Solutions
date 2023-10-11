4c1ec8f0b2c9ae5f961ad6724ad7fb2e0e691f73
sterf-mac <stefhwin@gmail.com>
2023-10-10 23:04:50 -0400

    trees e

---
+++ L617-MergeTwoTrees.py
@@ -0,0 +1,16 @@
+# 617 - Merge TWo Binary Trees
+
+"""
+
+
+"""
+
+for num in range(1, 101):
+    if num % 5 == 0 and num % 3 == 0:
+        print("CracklePop")
+    elif num % 5 == 0:
+        print("Pop")
+    elif num % 3 == 0:
+        print("Crackle")
+    else:
+        print(num)
